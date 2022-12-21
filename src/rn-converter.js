c656907d10d7f4302665ab8540e7f0d05a69d20e
Kirsty Leishman <leishman.kirsty@gmail.com>
2022-12-21 10:55:07 +0000

    changes from kata recording

--- src/rn-converter.js
+++ src/rn-converter.js
@@ -1,3 +1,24 @@
 export function converter(numeral) {
-    
+    // assume that numeral is a string and is a valid roman numeral
+    const numeralDecimal = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    const chars = numeral.split("");
+
+    return chars.reduce((acc, curr, i) => {
+        if (numeralDecimal[curr] <= numeralDecimal[chars[i - 1]] || i === 0) {
+            acc += numeralDecimal[curr];
+        }
+        else {
+            acc = acc + numeralDecimal[curr] - (2 * numeralDecimal[chars[i - 1]]);
+        }
+        return acc;
+    }, 0);
 };
--- src/rn-converter.test.js
+++ src/rn-converter.test.js
@@ -1,5 +1,24 @@
 import { converter } from './rn-converter.js';
 
-test('takes I and converts to 1', () => {
+test('takes a one unit numeral and correctly converts', () => {
     expect(converter('I')).toBe(1);
-})
+    expect(converter('V')).toBe(5);
+    expect(converter('X')).toBe(10);
+    expect(converter('L')).toBe(50);
+    expect(converter('C')).toBe(100);
+    expect(converter('D')).toBe(500);
+    expect(converter('M')).toBe(1000);
+});
+
+test('takes a two unit numeral and correctly converts', () => {
+    expect(converter('II')).toBe(2);
+    expect(converter('VI')).toBe(6);
+    expect(converter('IV')).toBe(4);
+    expect(converter('IX')).toBe(9);
+    expect(converter('IL')).toBe(49);
+});
+
+test('takes a numeral over 2 in length and correctly converts', () => {
+    expect(converter('III')).toBe(3);
+    expect(converter('XCIX')).toBe(99);
+});
